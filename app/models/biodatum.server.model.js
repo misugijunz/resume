d1513c272634155f90b2ac789b5c51c87c21e507
Oscar Kurniawan <misugijunz@gmail.com>
2015-07-08 00:09:30 +0700

    biodata model
    adding fields to biodata model

--- app/models/biodatum.server.model.js
+++ app/models/biodatum.server.model.js
@@ -5,6 +5,7 @@
  */
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
+require('mongoose-type-email');
 
 /**
  * Biodatum Schema
@@ -13,12 +14,36 @@ var BiodatumSchema = new Schema({
 	name: {
 		type: String,
 		default: '',
-		required: 'Please fill Biodatum name',
+		required: 'Please enter your name.',
 		trim: true
+	},
+	dateOfBirth: {
+		type: Date,
+		default: Date.now	
+	},
+	address: {
+		type: String,
+		default: '',
+		required: 'Please enter your address.'
+	},
+	 email: {
+        work: {type: mongoose.SchemaTypes.Email, required: true},
+        home: {type: mongoose.SchemaTypes.Email, required: true},
+    },
+	phones: {
+		work: {type: String, required: true},
+		home: {type: String, required: true},
 	},
 	created: {
 		type: Date,
 		default: Date.now
+	},
+	avatar: {
+		href: {type: String},	
+	},
+	socials: {
+		skype: {type: String},
+		linkedin: {type: String},	
 	},
 	user: {
 		type: Schema.ObjectId,
